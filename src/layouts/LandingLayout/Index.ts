f6b3cd7929fdcc2ec51e1dfc393a64e3dd1e957c
Abigail <66679101+abigailojeda@users.noreply.github.com>
2026-08-13 21:49:24 +0200

    feat: apply landing page theme

--- src/layouts/LandingLayout/Index.ts
+++ src/layouts/LandingLayout/Index.ts
@@ -1,5 +1,14 @@
-import { defineComponent } from 'vue'
+import { defineComponent, onMounted } from 'vue'
+
+import { applyTheme } from '@/theme/applyTheme'
+import { growbaseTheme } from '@/theme/growbase.theme'
 
 export default defineComponent({
   name: 'LandingLayout',
+
+  setup() {
+    onMounted(() => {
+      applyTheme(growbaseTheme.light)
+    })
+  },
 })
