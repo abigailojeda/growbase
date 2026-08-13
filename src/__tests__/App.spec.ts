b9fb5e39cec5f6523628d56ffcce4cce423e1e48
Abigail <66679101+abigailojeda@users.noreply.github.com>
2026-08-13 19:25:06 +0200

    chore: clean up default setup and prepare application structure

--- src/__tests__/App.spec.ts
+++ src/__tests__/App.spec.ts
@@ -1,11 +1,32 @@
-import { describe, it, expect } from 'vitest'
-
 import { mount } from '@vue/test-utils'
-import App from '../App.vue'
+import { createMemoryHistory, createRouter } from 'vue-router'
+import { describe, expect, it } from 'vitest'
+
+import App from '@/App.vue'
 
 describe('App', () => {
-  it('mounts renders properly', () => {
-    const wrapper = mount(App)
-    expect(wrapper.text()).toContain('You did it!')
+  it('renders the current route', async () => {
+    const router = createRouter({
+      history: createMemoryHistory(),
+      routes: [
+        {
+          path: '/',
+          component: {
+            template: '<div>Test page</div>',
+          },
+        },
+      ],
+    })
+
+    await router.push('/')
+    await router.isReady()
+
+    const wrapper = mount(App, {
+      global: {
+        plugins: [router],
+      },
+    })
+
+    expect(wrapper.text()).toContain('Test page')
   })
 })
